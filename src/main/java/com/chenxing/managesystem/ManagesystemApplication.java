4040dab59ef895a1b5987b762fa205c42938f005
huayu <huayu@DESKTOP-N5K2GI1>
2018-09-13 17:34:45 +0800

    aaaa

--- src/main/java/com/chenxing/managesystem/ManagesystemApplication.java
+++ src/main/java/com/chenxing/managesystem/ManagesystemApplication.java
@@ -2,15 +2,24 @@ package com.chenxing.managesystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
-public class ManagesystemApplication {
+public class ManagesystemApplication extends SpringBootServletInitializer
+		implements EmbeddedServletContainerCustomizer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManagesystemApplication.class, args);
+	}
+
+	@Override
+	public void customize(ConfigurableEmbeddedServletContainer configurableEmbeddedServletContainer) {
+		configurableEmbeddedServletContainer.setContextPath("/mycas");
 	}
 }
